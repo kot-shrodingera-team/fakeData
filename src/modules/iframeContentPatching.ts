9fa2d0415c060a11308578884746033f500bd804
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-01-24 17:28:22 +0100

    Условие патча плагинов во фреймах в зависимости от рекапчи

--- src/modules/iframeContentPatching.ts
+++ src/modules/iframeContentPatching.ts
@@ -26,7 +26,9 @@ const iframeContentPatching = (fakeProfile: FakeProfile): void => {
         }
         patchCanvas(a, fakeProfile);
         patchWindow(a, fakeProfile, true);
-        // patchPlugins(a, true);
+        if (document.querySelector('#recaptcha-token')) {
+          // patchPlugins(a, true);
+        }
         patchDnt(a, fakeProfile);
         patchAudio(a, fakeProfile);
         patchGeo(a, fakeProfile);
@@ -47,7 +49,9 @@ const iframeContentPatching = (fakeProfile: FakeProfile): void => {
         }
         patchCanvas(a, fakeProfile);
         patchWindow(a, fakeProfile);
-        // patchPlugins(a);
+        if (document.querySelector('#recaptcha-token')) {
+          // patchPlugins(a);
+        }
         patchDnt(a, fakeProfile);
         patchAudio(a, fakeProfile);
         patchGeo(a, fakeProfile);
